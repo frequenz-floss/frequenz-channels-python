e6cf737b2f6e690a7595763fd47c21033dc75134
Leandro Lucarella <leandro.lucarella@frequenz.com>
2023-05-05 15:38:00 +0200

    Add test for the FileWatcher event filters
    Signed-off-by: Leandro Lucarella <leandro.lucarella@frequenz.com>

--- tests/utils/test_file_watcher.py
+++ tests/utils/test_file_watcher.py
@@ -10,6 +10,8 @@ from collections.abc import AsyncGenerator, Iterator, Sequence
 from typing import Any
 from unittest import mock
 
+import hypothesis
+import hypothesis.strategies as st
 import pytest
 from watchfiles import Change
 from watchfiles.main import FileChange
@@ -70,3 +72,30 @@ async def test_file_watcher_receive_updates(
     for change in changes:
         recv_changes = await file_watcher.receive()
         assert recv_changes == pathlib.Path(change[1])
+
+
+@hypothesis.given(event_types=st.sets(st.sampled_from(FileWatcher.EventType)))
+async def test_file_watcher_filter_events(
+    event_types: set[FileWatcher.EventType],
+) -> None:
+    """Test the file watcher events filtering."""
+    good_path = "good-file"
+
+    # We need to reset the mock explicitly because hypothesis runs all the produced
+    # inputs in the same context.
+    with mock.patch(
+        "frequenz.channels.util._file_watcher.awatch", autospec=True
+    ) as awatch_mock:
+        file_watcher = FileWatcher(paths=[good_path], event_types=event_types)
+
+        filter_events = file_watcher._filter_events  # pylint: disable=protected-access
+
+        assert awatch_mock.mock_calls == [
+            mock.call(
+                pathlib.Path(good_path), stop_event=mock.ANY, watch_filter=filter_events
+            )
+        ]
+        for event_type in FileWatcher.EventType:
+            assert filter_events(event_type.value, good_path) == (
+                event_type in event_types
+            )
