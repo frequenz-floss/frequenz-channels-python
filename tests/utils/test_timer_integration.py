fca8a852af6bddfcf631171d3d807507813857a2
Leandro Lucarella <luca-frequenz@llucax.com>
2023-11-21 12:44:29 +0100

    Add test for `reset()` while a timer is being waited on
    This test needs to be an integration test because async-solipsism would
    make any sleep return immediately, so we wouldn't have a way to do the
    reset while the timer is being waited on.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

---
+++ tests/utils/test_timer_integration.py
@@ -0,0 +1,34 @@
+# License: MIT
+# Copyright © 2023 Frequenz Energy-as-a-Service GmbH
+
+"""Integration tests for the timer."""
+
+
+import asyncio
+from datetime import timedelta
+
+import async_solipsism
+import pytest
+
+from frequenz.channels.util import Timer
+
+
+@pytest.mark.integration
+async def test_timer_timeout_reset(
+    event_loop: async_solipsism.EventLoop,  # pylint: disable=redefined-outer-name
+) -> None:
+    """Test that the receiving is properly adjusted after a reset."""
+
+    async def timer_wait(timer: Timer) -> None:
+        await timer.receive()
+
+    async with asyncio.timeout(2.0):
+        async with asyncio.TaskGroup() as task_group:
+            timer = Timer.timeout(timedelta(seconds=1.0))
+            start_time = event_loop.time()
+            task_group.create_task(timer_wait(timer))
+            await asyncio.sleep(0.5)
+            timer.reset()
+
+    run_time = event_loop.time() - start_time
+    assert run_time >= 1.5
